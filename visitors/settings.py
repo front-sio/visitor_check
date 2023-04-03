1b21f0a5a56e6c4a9ffb0cff0c8ba06718c1f27c
masanja joseph <54031264+front-sio@users.noreply.github.com>
2023-04-03 16:37:10 +0300

    deploy

--- visitors/settings.py
+++ visitors/settings.py
@@ -21,9 +21,11 @@ import dj_database_url
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!tjurmzeh^2zrw*=&*d_v+n59y+89bi#&0@ff^vyoaq8%=s0q3'
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -83,7 +85,6 @@ WSGI_APPLICATION = 'visitors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
