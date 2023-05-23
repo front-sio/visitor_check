9d11bb1caf203aa615257621ec902f1540d17897
masanja joseph <54031264+front-sio@users.noreply.github.com>
2023-05-23 13:37:10 +0300

    static settings

--- visitors/settings.py
+++ visitors/settings.py
@@ -164,6 +164,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
